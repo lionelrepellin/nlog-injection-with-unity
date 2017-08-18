daf380dbca1215b6a27685da00be10b4eef06c7b
Lionel <lionel.repellin@gmail.com>
2017-08-18 14:45:12 +0200

    Ajout du service de log dans Unity + dans le Ctrl

--- TestInjectNlog/App_Start/UnityConfig.cs
+++ TestInjectNlog/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.Configuration;
+using TestInjectNlog.Logger;
 
 namespace TestInjectNlog.App_Start
 {
@@ -32,11 +33,12 @@ namespace TestInjectNlog.App_Start
         /// change the defaults), as Unity allows resolving a concrete type even if it was not previously registered.</remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
-            // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
-            // container.LoadConfiguration();
+			// NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
+			// container.LoadConfiguration();
 
-            // TODO: Register your types here
-            // container.RegisterType<IProductRepository, ProductRepository>();
+			// TODO: Register your types here
+			// container.RegisterType<IProductRepository, ProductRepository>();
+			container.RegisterType<ILoggerService, LoggerService>();
         }
     }
 }
--- TestInjectNlog/Controllers/HomeController.cs
+++ TestInjectNlog/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestInjectNlog.Logger;
 
 namespace TestInjectNlog.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly ILoggerService _loggerService;
+
+		public HomeController(ILoggerService loggerService)
+		{
+			_loggerService = loggerService;
+		}
+
+
 		public ActionResult Index()
 		{
 			ViewBag.Title = "Home Page";
+
+			_loggerService.Debug("Message à la con...");
 
 			return View();
 		}
